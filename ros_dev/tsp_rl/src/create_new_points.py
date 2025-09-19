2d3a543f65dc88257b3b8efb7efd139d3be95d2b
rohan-w123 <rwadhwa@terpmail.umd.edu>
2025-09-19 18:19:57 -0400

    Updated

--- ros_dev/tsp_rl/src/create_new_points.py
+++ ros_dev/tsp_rl/src/create_new_points.py
@@ -10,18 +10,12 @@ with open(".../credentials.json") as f:
 gmaps = googlemaps.Client(key=creds["gmaps"])
 
 file = pd.read_excel("Dataset.xlsx", engine="openpyxl")
-
 new_df = pd.DataFrame(file)
 new_df.index = np.arange(1, len(new_df) + 1)
-
 new_df = new_df.drop_duplicates()
-
 new_df = new_df.dropna()
-
-
 new_df["Long"] = 0
 new_df["Lat"] = 0
-
 for i in range(len(new_df)):
     address = new_df.iloc[i,0]
     geocoded_result = gmaps.geocode(address)
@@ -34,6 +28,7 @@ for i in range(len(new_df)):
 
 print(new_df)
 
+#calculates the weather adjusted speed using Directions API for various routes
 def get_weather_speed(origin, destination, gmaps):
     # Weather impact from traffic congestion variability in Directions API
     result = gmaps.directions(
@@ -53,11 +48,10 @@ def get_weather_speed(origin, destination, gmaps):
         return 0.8 if ratio > 1.2 else 1.0
     return 1.0
 
+#Applies the weather adjusted feed process to specific start and end point
 def load_location_nodes(credentials_file):
-    with open(credentials_file) as f:
-        creds = json.load(f)
     global new_df
-    nodes = [(row["Lat"], row["long"]) for _, row in new_df.iterrows()]
+    nodes = [(row["Lat"], row["Long"]) for _, row in new_df.iterrows()]
 
     if len(nodes) > 1:
         speed = get_weather_speed((nodes[0][0], nodes[0][1]), (nodes[1,0], nodes[1,1]), gmaps)
@@ -65,10 +59,39 @@ def load_location_nodes(credentials_file):
         speed = 1.0
     return nodes[:10], speed
 
-def get_directions_matrix(origins, destinations, gmaps):
-    #Fetches three routes based on Google's Directions API
+def get_directions_matrix(nodes, gmaps):
+    #Fetches traffic aware travel times using Distance Matrix API and Caches them
+    cache_file = "../cache/distance_matrix.json"
+    try:
+        with open(cache_file, "r") as f:
+            return json.load(f)
+    except: 
+        matrix = gmaps.distance_matrix(origins=nodes, destinations=nodes, mode="driving", departure_time = "now")
+        with open(cache_file, "w") as f:
+            json.dump(matrix, f)
+    return matrix
 
 
+def validate_nodes(nodes):
+    if len(nodes) != 10 or len(set(nodes)) != 10:
+        raise ValueError("Must have 10 nodes")
+    return nodes
+
+#Generates a complete TSP instance with nodes, distances, and speed
+#Needed for initialization of RL training data and dynamic replanning
+def generate_tsp_instance(credentials_file):
+    nodes, speed = load_location_nodes(credentials_file)
+    global new_df
+    with open(credentials_file) as f:
+        gmaps = googlemaps.Client(key=json.load(f)["map_api_key"])
+    distance_matrix = get_directions_matrix(nodes, gmaps)
+    return validate_nodes(nodes), distance_matrix, speed, new_df
+
+if __name__ == "__main__":
+    credentials_file = "../credentials.json"
+    nodes, distance_matrix, speed, new_df = generate_tsp_instance(credentials_file)
+    print("Nodes: ", nodes, "Speed: ", speed)
+    print("Dataframe: ", new_df)
 
 
 
